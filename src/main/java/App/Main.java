fbbfa66050c2a38c76ae8ab36c64d313740bc102
Iv44n <ivandev2oo6@gmail.com>
2024-11-20 21:37:47 -0500

    refactor: update Main to launch SignIn view

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,8 +1,10 @@
 package App;
 
+import Views.Auth.SignIn;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new SignIn().setVisible(true);
     }
 }
